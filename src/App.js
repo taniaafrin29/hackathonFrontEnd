cc9849dd6997105c56de3b2de803cc2abd51ceeb
Sk Tania Afrin <Tania@Sks-MacBook-Pro.local>
2019-10-19 15:08:22 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Card from './Card';
+import data from './data/data'
+import logo from './assets/logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class component
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      properties: data.properties,
+      property: data.properties[0]
+    }
+  }
+
+  nextProperty = () => {
+    const newIndex = this.state.property.index+1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  prevProperty = () => {
+    const newIndex = this.state.property.index-1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  render() {
+    const {property} = this.state;
+    return (
+      <div className="App">
+
+        <button 
+          onClick={() => this.nextProperty()} 
+          disabled={property.index === data.properties.length-1}
+        >Next</button>
+        <button 
+          onClick={() => this.prevProperty()} 
+          disabled={property.index === 0}
+        >Prev</button>
+
+        <div className="page">
+            <section>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Image slideshow React tutorial.</h1>
+            </section>
+
+            <Card property={property} />
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
